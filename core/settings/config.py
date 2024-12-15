62ee7e9f0def90cea59c23fb8443db8a4daff972
admin <denis@1>
2024-12-15 22:25:15 +0700

    after .gitignore added

--- core/settings/config.py
+++ core/settings/config.py
@@ -4,6 +4,7 @@ from enum import Enum
 class Users(Enum):
     ADMIN_USERNAME = "admin"
     ADMIN_PASSWORD = "Test18plat34Form"
+    ADMIN_GRANT_TYPE = 'password'
 
     USER_USERNAME = "user"
     USER_PASSWORD = "admin"
