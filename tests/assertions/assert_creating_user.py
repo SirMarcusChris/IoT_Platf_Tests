1a6be19c0022dc71cce849ea765ee1467ba0c84f
admin <denis@1>
2025-01-20 00:05:58 +0700

    продолжение написания тестов и продумываение логики

--- tests/assertions/assert_creating_user.py
+++ tests/assertions/assert_creating_user.py
@@ -3,15 +3,18 @@ import json
 import requests
 
 def assert_creating_user(response):
+    response_json = response.json()
     assert response.status_code == 200
     assert isinstance(response.json()['id'], str)
     assert 'id' in response.json()
 
 def assert_get_users_list(response):
-    assert response.status_code == 200
     response_json = response.json()
+    assert response.status_code == 200
     assert 'data' in response_json
     assert isinstance(response_json['data'], list)
     # Проверяем, что в каждом элементе списка 'data' есть 'id'
     for users in response_json['data']:
         assert 'id' in users
+    assert response_json['total'] > 1
+    assert any(user["roleName"] == "Абонент" for user in response_json["data"])
